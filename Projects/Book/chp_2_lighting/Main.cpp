8e04f6cb6ee34883437ffe6b5e0f0749200640fe
Bokhtiar-Adil <prottoy1807028@stud.kuet.ac.bd>
2023-10-06 08:44:39 +0600

    Before 14.3

--- Projects/Book/chp_2_lighting/Main.cpp
+++ Projects/Book/chp_2_lighting/Main.cpp
@@ -178,6 +178,13 @@ int main()
         lightingShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
         lightingShader.setVec3("lightPos", lightPos);
         lightingShader.setVec3("viewPos", camera.Position);
+        lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+        lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+        lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+        lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+        lightingShader.setFloat("material.shininess", 32.0f);
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f); 
@@ -187,6 +194,7 @@ int main()
 
         // world transformation
         glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.7f, 0.5f, 0.0f));
         lightingShader.setMat4("model", model);
 
         // render the cube
--- Projects/Book/chp_2_lighting/fsSrc.fs
+++ Projects/Book/chp_2_lighting/fsSrc.fs
@@ -1,8 +1,27 @@
 #version 330 core
+
+
+struct Light {
+    vec3 position;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+struct Material {
+    vec3 ambient; // K_amb
+    vec3 diffuse; // K_diff
+    vec3 specular; // K_spec
+    float shininess; // n
+};
+
+
 out vec4 FragColor;
 in vec3 Normal;
 in vec3 FragPos; 
 
+uniform Light light;
+uniform Material material;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 uniform vec3 lightPos;
@@ -10,21 +29,22 @@ uniform vec3 viewPos;
 
 void main()
 {
+    // ambient
+    vec3 ambient = light.ambient * material.ambient; // I_amb = K_amb * I_src
+    
+    // diffuse
     vec3 norm = normalize(Normal);
-    
-    float ambientStr = 0.1f;
-    vec3 ambient = ambientStr * lightColor; // I_amb = K_amb * I_src
-    
     vec3 lightDir = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDir), 0.0f); // only positive values of normalized (normal * light_direction)
-    vec3 diffuse = diff * lightColor; // I_diff = (normal * light_direction) * I_src    
+    vec3 diffuse = light.diffuse * (diff * material.diffuse); // I_diff = K_diff * (normal * light_direction) * I_src    
 
+    // specular
     float specularStr = 0.5f;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm); // negate it as lightDir is obj to src -> need src to obj
-    float spec = pow(max(dot(reflectDir, viewDir), 0.0f), 32.0f);
-    vec3 specular = specularStr * spec * lightColor; // (reflection_direction * normal)^n * I_src
+    float spec = pow(max(dot(reflectDir, viewDir), 0.0f), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular); // I_spec = k_spec * (reflection_direction * normal)^shininess * I_src
 
-    FragColor = vec4((ambient + diffuse + specular) * objectColor, 1.0f); // I_obj = K_amb * I_amb + K_diff * I_diff + K_spec * I_spec
+    FragColor = vec4((ambient + diffuse + specular), 1.0f); // I_obj = I_amb + I_diff + I_spec
 
 }
